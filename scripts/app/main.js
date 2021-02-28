65224927bbf1ec05c9489cb968e458b3b63ceb7e
wmik <williemik.wmik@gmail.com>
2021-02-28 12:50:36 +0300

    feat: adsr envelope - Setup ADSR envelope - Update components API - Bug fixes - Manual test

--- scripts/app/main.js
+++ scripts/app/main.js
@@ -2,15 +2,16 @@ import createSynthesizer from '../lib/synth';
 
 let synth = createSynthesizer({
   master: { volume: 1, pan: 0 },
-  oscillator: { wave: 'sine' }
+  oscillator: { wave: 'sine' },
+  envelope: { attack: 0.2, decay: 0.02, sustain: 0.5, release: 0.5 }
 });
 
 let a4 = document.getElementById('a4');
 
 a4.addEventListener('mousedown', () => {
-  synth.start(440);
+  synth.start(440, 'a4');
 });
 
 a4.addEventListener('mouseup', () => {
-  synth.stop();
+  synth.stop('a4');
 });
--- scripts/lib/synth.js
+++ scripts/lib/synth.js
@@ -1,10 +1,19 @@
+const THRESHOLD = 0.0001;
+const MINIMUM = 0.000001;
+
 /**
  *
  * @param {AudioContext} ctx
  * @param {GainOptions} config
  */
 function createAmplifier(ctx, config) {
-  return new GainNode(ctx, config);
+  let node = new GainNode(ctx, config);
+
+  function connect(next) {
+    node.connect(next.node);
+  }
+
+  return { connect, node };
 }
 
 /**
@@ -13,7 +22,13 @@ function createAmplifier(ctx, config) {
  * @param {StereoPannerOptions} config
  */
 function createPanner(ctx, config) {
-  return new StereoPannerNode(ctx, config);
+  let node = new StereoPannerNode(ctx, config);
+
+  function connect(next) {
+    node.connect(next.node);
+  }
+
+  return { connect, node };
 }
 
 /**
@@ -22,13 +37,75 @@ function createPanner(ctx, config) {
  * @param {OscillatorOptions} config
  */
 function createOscillator(ctx, config) {
-  return new OscillatorNode(ctx, config);
+  let node = new OscillatorNode(ctx, config);
+
+  function start(frequency) {
+    node.frequency.setValueAtTime(frequency, ctx.currentTime);
+    node.start();
+  }
+
+  function stop(time) {
+    node.stop(time);
+  }
+
+  function connect(next) {
+    node.connect(next.node);
+  }
+
+  return { start, stop, connect, node };
+}
+
+/**
+ * @typedef {Object} EnvelopeOptions
+ * @property {number} attack
+ * @property {number} decay
+ * @property {number} sustain
+ * @property {number} release
+ *
+ * @param {AudioContext} ctx
+ * @param {EnvelopeOptions} config
+ */
+function createEnvelope(ctx, config) {
+  let node = new GainNode(ctx, { gain: MINIMUM });
+
+  function start() {
+    let peakTime = ctx.currentTime + config.attack + THRESHOLD;
+
+    node.gain.exponentialRampToValueAtTime(1.0, peakTime);
+    node.gain.exponentialRampToValueAtTime(
+      config.sustain + THRESHOLD,
+      peakTime + config.decay
+    );
+  }
+
+  function stop() {
+    // uses logarithmic scale - minimum can't be 0 (get very close to 0)
+    // log scale mimics human ear perception of sound (i.e. pitches)
+    // also trying to eliminate annoying click at end of note release 🤦‍♂️
+    node.gain.setValueAtTime(node.gain.value, ctx.currentTime);
+    node.gain.exponentialRampToValueAtTime(
+      MINIMUM,
+      ctx.currentTime + Math.max(config.release, THRESHOLD)
+    );
+  }
+
+  function connect(next) {
+    node.connect(next.node);
+  }
+
+  return { start, stop, connect, node };
 }
 
 /**
  * @typedef {Object} MasterOptions
  * @property {number} volume
  * @property {number} pan
+ *
+ * @typedef {Object} EnvelopeOptions
+ * @property {number} attack
+ * @property {number} decay
+ * @property {number} sustain
+ * @property {number} release
  *
  * @typedef {Object} OscillatorOptions
  * @property {('sine'|'triangle'|'sawtooth'|'square')} wave
@@ -45,46 +122,35 @@ export default function createSynthesizer(config) {
   function start(frequency, key) {
     let ctx = new AudioContext();
     let pan = createPanner(ctx, { pan: config.master.pan });
-    let amp = createAmplifier(ctx, { gain: 0.0 });
+    let amp = createAmplifier(ctx, { gain: config.master.volume });
+    let envelope = createEnvelope(ctx, config.envelope);
     let osc = createOscillator(ctx, { type: config.oscillator.wave });
 
     osc.connect(amp);
-    amp.connect(pan);
-    pan.connect(ctx.destination);
-    osc.start();
+    amp.connect(envelope);
+    envelope.connect(pan);
+    pan.connect({ node: ctx.destination });
 
-    osc.frequency.setValueAtTime(frequency, ctx.currentTime);
-    amp.gain.exponentialRampToValueAtTime(
-      config.master.volume,
-      ctx.currentTime
-    );
+    envelope.start();
+    osc.start(frequency);
 
     // components aka nodes are single use only - create & cache for each note
-    nodes[key] = { osc, amp, ctx };
+    nodes[key] = { osc, ctx, envelope };
   }
 
-  function stop(time, key) {
-    const THRESHOLD = 0.0001;
-
+  function stop(key) {
     let node = nodes[key];
 
     if (node) {
-      node.amp.gain.setValueAtTime(node.amp.gain.value, node.ctx.currentTime);
-      // uses logarithmic scale - minimum can't be 0 (get very close to 0)
-      // log scales mimics human ear perception of sound (i.e. pitches)
-      // also trying to eliminate annoying click at end of note release 🤦‍♂️
-      node.amp.gain.exponentialRampToValueAtTime(
-        0.000001,
-        node.ctx.currentTime
-      );
-      node.osc.stop(node.ctx.currentTime + (time ?? 0));
+      node.envelope.stop();
+      node.osc.stop(node.ctx.currentTime + config.envelope.release);
+
+      setTimeout(() => {
+        node.ctx.close();
+      }, 1000 * Math.max(config.envelope.release, THRESHOLD));
     }
 
     delete nodes[key];
-
-    setTimeout(() => {
-      node.ctx.close();
-    }, 1000 * Math.min(time, THRESHOLD));
   }
 
   return { start, stop };
